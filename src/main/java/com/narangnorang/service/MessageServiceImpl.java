781ab7eb0e91bcb7a8f702b600e486e878ee85e7
jaengs <jaengsjaengs@naver.com>
2022-06-03 10:32:13 +0900

    feat:Message Alarm - modify table
    need to display alarms on top.jsp

--- src/main/java/com/narangnorang/service/MessageServiceImpl.java
+++ src/main/java/com/narangnorang/service/MessageServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.narangnorang.dao.MessageDAO;
 import com.narangnorang.dto.MessageDTO;
@@ -16,18 +17,25 @@ public class MessageServiceImpl implements MessageService {
 	MessageDAO messageDAO;
 
 	@Override
-	public List<MessageDTO> selectMessageList(int id) throws Exception {
-		return messageDAO.selectMessageList(id);
+	public List<MessageDTO> selectMessageList(int userId) throws Exception {
+		return messageDAO.selectMessageList(userId);
 	}
 
 	@Override
 	public int sendMessage(Map<String, Object> messageInfo) throws Exception {
 		return messageDAO.sendMessage(messageInfo);
 	}
-
+	
+	@Transactional
 	@Override
 	public List<MessageDTO> getChats(Map<String, Object> map) throws Exception {
+		messageDAO.readMessage(map);
 		return messageDAO.getChats(map);
+	}
+
+	@Override
+	public int countUnread(int userId) throws Exception {
+		return messageDAO.countUnread(userId);
 	}
 
 
--- src/main/webapp/WEB-INF/views/message.jsp
+++ src/main/webapp/WEB-INF/views/message.jsp
@@ -5,7 +5,7 @@
 <head>
 <meta charset="UTF-8">
 <title>나랑노랑</title>
-<script type="text/javascript" src="/narangnorang/js/message/messageList.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 <body>
 	<h1>쪽지함</h1>
@@ -13,5 +13,6 @@
 	<jsp:include page="common/navBar.jsp" flush="true" /><br>
 	<hr>
 	<jsp:include page="message/messageList.jsp" flush="true"/>
+	<script type="text/javascript" src="/narangnorang/js/message/messageList.js"></script>
 </body>
 </html>
